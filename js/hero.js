c00a3a2b03f226f619113ad317923d494a93caca
Kevin Ahn <maketheworldwise@gmail.com>
2022-07-14 13:15:18 +0900

    :sparkles: hero 위치 및 방향 js 조작 테스트

---
+++ js/hero.js
@@ -0,0 +1,6 @@
+const heroElement = document.getElementById("hero");
+console.log(heroElement);
+
+// hero id를 가진 Element의 클래스를 변경
+heroElement.className = "right";
+heroElement.style.left = "30px";
