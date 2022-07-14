7ca523acc08a3293314291d22d3d4876145fa7df
Kevin Ahn <maketheworldwise@gmail.com>
2022-07-14 20:01:37 +0900

    :sparkles: ghost 랜덤 위치 적용

--- js/ghost.js
+++ js/ghost.js
@@ -1,11 +1,22 @@
-const ghostElement = document.createElement("span");
+function createGhost() {
+  const ghostElement = document.createElement("span");
 
-ghostElement.style.position = "absolute";
-ghostElement.style.top = "0px";
-ghostElement.style.left = "50%";
+  ghostElement.style.position = "absolute";
+  ghostElement.style.top = "0px";
 
-ghostElement.style.width = GHOST_WIDTH + "px";
-ghostElement.style.height = GHOST_HEIGHT + "px";
-ghostElement.style.background = 'url("./images/ghost.png") no-repeat';
+  let randomLeft = randomNum();
+  ghostElement.style.left = randomLeft + "px"; // "50%";
 
-bgElement.appendChild(ghostElement);
+  ghostElement.style.width = GHOST_WIDTH + "px";
+  ghostElement.style.height = GHOST_HEIGHT + "px";
+  ghostElement.style.background = 'url("./images/ghost.png") no-repeat';
+
+  bgElement.appendChild(ghostElement);
+}
+
+createGhost();
+
+function randomNum() {
+  let randomNumberUnder755 = Math.floor(Math.random() * 755);
+  return randomNumberUnder755;
+}
