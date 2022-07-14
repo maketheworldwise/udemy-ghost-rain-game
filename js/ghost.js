ffbc5b0159e8fd53691a8846ea5583142ebedaed
Kevin Ahn <maketheworldwise@gmail.com>
2022-07-15 02:40:39 +0900

    :sparkles: ghost 이동
    - 반복적으로 ghost 랜덤한 위치에 생성 및 이동
    - ghost가 범위를 넘어설 경우의 처리
    - hero와 만났을 때의 처리

--- js/ghost.js
+++ js/ghost.js
@@ -12,10 +12,47 @@ function createGhost() {
   ghostElement.style.background = 'url("./images/ghost.png") no-repeat';
 
   bgElement.appendChild(ghostElement);
+
+  setInterval(function () {
+    const ghostTopPoint = removePxString(ghostElement.style.top);
+    const ghostLeftPoint = removePxString(ghostElement.style.left);
+    const heroLeftPoint = removePxString(heroElement.style.left);
+
+    if (ghostTopPoint >= BG_HEIGHT - (HERO_HEIGHT + GHOST_HEIGHT)) {
+      const ghostPointRange = [ghostLeftPoint, ghostLeftPoint + GHOST_WIDTH];
+      const heroPointRange = [heroLeftPoint, heroLeftPoint + HERO_WIDTH];
+
+      if (
+        (ghostPointRange[0] < heroPointRange[0] &&
+          heroPointRange[0] < ghostPointRange[1]) ||
+        (ghostPointRange[0] < heroPointRange[1] &&
+          heroPointRange[1] < ghostPointRange[1])
+      ) {
+        killGhost(ghostElement);
+        return;
+      }
+    }
+
+    const move = ghostTopPoint + 10;
+
+    if (move >= BG_HEIGHT - GHOST_HEIGHT) {
+      ghostElement.remove();
+      return;
+    }
+
+    ghostElement.style.top = move + "px";
+  }, 100);
+}
+
+function killGhost(ghostElement) {
+  ghostElement.style.backgroundPosition = "-45px 0px";
+  setTimeout(function () {
+    ghostElement.remove();
+  }, 300);
 }
 
 function randomNum(min, max) {
   return Math.floor(Math.random() * (max + 1 - min)) + min;
 }
 
-createGhost();
+setInterval(createGhost, 300);
